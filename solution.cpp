a6f6cdf9dfc422f8ad9df74d8fdf07e63dd76f60
peppapig450 <nbgenius1@gmail.com>
2024-07-16 12:00:32 -0700

    solve the first two sum in python and c++

---
+++ solution.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        std::unordered_map<int, int> seen;
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            int complement = target - nums[i];
+
+            if (seen.count(complement))
+            {
+                return {seen[complement], i};
+            }
+
+            seen[nums[i]] = i;
+        }
+        return {};
+    }
+};
