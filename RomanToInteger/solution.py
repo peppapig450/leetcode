efda54c79c1b34ed8c2e901e80b7384d6f6539bb
peppapig450 <nbgenius1@gmail.com>
2024-07-23 12:15:52 -0700

    solve problem 13 , roman to integer in python

---
+++ RomanToInteger/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_mapping = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+
+        result = 0
+        prev_value = 0
+        
+        for char in reversed(s):
+            value = roman_mapping[char]
+            if value < prev_value:
+                result -= value
+            else:
+                result += value
+            prev_value = value
+
+        return result
